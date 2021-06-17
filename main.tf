bbc6f8b28ca431ae61688c9714817f35e28277c5
Nico Vibert <nvibert@vmware.com>
2021-06-17 16:27:10 +0100

    working version

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
 
 # Configure the AWS Provider
 provider "aws" {
@@ -23,7 +15,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "eu-west-2a"
 
   tags = {
     Name = "tf-example"
@@ -40,8 +32,8 @@ resource "aws_network_interface" "foo" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
+  ami           = "ami-089539692cca55c6c" # eu-west-2
+  instance_type = "c4.4xlarge"
 
   network_interface {
     network_interface_id = aws_network_interface.foo.id
@@ -51,4 +43,9 @@ resource "aws_instance" "foo" {
   credit_specification {
     cpu_credits = "unlimited"
   }
+
+  tags = {
+    Name = "nico-terraform"
+  }
+
 }
