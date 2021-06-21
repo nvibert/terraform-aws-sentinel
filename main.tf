b179f1a3ceb8bcebd31e773c65a66be6f15ab10b
Nico Vibert <nvibert@vmware.com>
2021-06-21 12:35:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
